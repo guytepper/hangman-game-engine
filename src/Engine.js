6bada7508a6fd87fd55d69a759fd4cd0b29ab6b9
guytepper <guytepper@gmail.com>
2018-01-05 22:00:41 +0200

    Added hiddenWord property
    Would be used instead of charactersMap for the hidden characters array.
    
    charactersMap would be used to save the word characters as an array.

--- src/Engine.js
+++ src/Engine.js
@@ -5,15 +5,17 @@ const DEFAULT_CONFIG = {
 };
 
 function Engine() {
-  this.word = '';
-  this.charactersMap = [];
-  this.config = DEFAULT_CONFIG;
+  this.word = ''; // The game word.
+  this.hiddenWord = []; // The hidden characters array, e.g. ['_', '_', '_', '_'] for 'Baby'.
+  this.charactersMap = []; // The characters array, e.g. ['B', 'a', 'b', 'y'] for 'Baby'.
+  this.config = DEFAULT_CONFIG; // The game config object.
 }
 
 Engine.prototype.newGame = function newGame(word, config) {
   this.word = word;
+  this.hiddenWord = Utils.createConcealArr(word, this.config.concealCharacter);
+  this.charactersMap = [...word];
   this.config = Object.assign({}, this.config, config);
-  this.charactersMap = Utils.createConcealArr(word, this.config.concealCharacter);
   return this;
 };
 
